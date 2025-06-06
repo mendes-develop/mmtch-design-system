eace2294648a7952af483b3aae4a08c45b63db2c
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2025-06-05 23:29:55 -0400

    css tokens work

--- packages/design-system/components/Button/Button.tsx
+++ packages/design-system/components/Button/Button.tsx
@@ -9,7 +9,7 @@ interface MyButtonProps {
 export const MyButton = ({ onPress, text }: MyButtonProps) => {
   return (
     <Pressable
-      className="self-center px-8 py-2 bg-sky-700 rounded-lg web:hover:bg-sky-600 native:active:bg-sky-600"
+      className="self-center px-8 py-2 bg-primary rounded-lg web:hover:bg-sky-600 native:active:bg-sky-600"
       onPress={onPress}
     >
       <Text className="text-white text-lg font-bold">{text}</Text>
