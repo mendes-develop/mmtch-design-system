029b6c4bc50bc9c77176da058c1970f7e9fb51eb
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2025-06-06 02:05:21 -0400

    Update Button.tsx

--- packages/design-system/components/Button/Button.tsx
+++ packages/design-system/components/Button/Button.tsx
@@ -3,16 +3,16 @@ import { Pressable, Text, StyleSheet } from "react-native";
 
 interface MyButtonProps {
   onPress: () => void;
-  text: string;
+  children: string;
 }
 
-export const MyButton = ({ onPress, text }: MyButtonProps) => {
+export const Button = ({ onPress, children }: MyButtonProps) => {
   return (
     <Pressable
-      className="self-center px-8 py-2 bg-primary rounded-lg web:hover:bg-sky-600 native:active:bg-sky-600"
+      className="self-center px-8 py-2 bg-primary-foreground rounded-lg web:hover:bg-sky-600 native:active:bg-sky-600"
       onPress={onPress}
     >
-      <Text className="text-primary-foreground text-lg font-bold">{text}</Text>
+      <Text className="text-primary-foreground text-lg font-bold">{children}</Text>
     </Pressable>
   );
 };
