9aba9501763e84e3927b69d915fca15d49b94a9c
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2025-06-07 21:45:05 -0400

    wip

--- packages/design-system/app.config.js
+++ packages/design-system/app.config.js
@@ -1,32 +1,45 @@
 export default ({ config }) => ({
-  ...config,
-  name: "exponativewindtemplatestorybook",
-  slug: "expo-template-blank-typescript",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
-  extra: {
-    storybookEnabled: process.env.STORYBOOK_ENABLED,
-  },
-  splash: {
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
-  },
-  updates: {
-    fallbackToCacheTimeout: 0,
-  },
-  assetBundlePatterns: ["**/*"],
-  ios: {
-    supportsTablet: true,
-  },
-  android: {
-    adaptiveIcon: {
-      foregroundImage: "./assets/adaptive-icon.png",
-      backgroundColor: "#FFFFFF",
-    },
-  },
-  web: {
-    favicon: "./assets/favicon.png",
-  },
+	...config,
+	name: 'exponativewindtemplatestorybook',
+	slug: 'expo-template-blank-typescript',
+	version: '1.0.0',
+	orientation: 'portrait',
+	icon: './assets/icon.png',
+	extra: {
+		storybookEnabled: true,
+		'eas': {
+			projectId: 'fd290ea5-8636-4cf1-965d-0da59c604ee0',
+		},
+	},
+	splash: {
+		image: './assets/splash.png',
+		resizeMode: 'contain',
+		backgroundColor: '#ffffff',
+	},
+	updates: {
+		fallbackToCacheTimeout: 0,
+	},
+	assetBundlePatterns: ['**/*'],
+	ios: {
+		supportsTablet: true,
+		bundleIdentifier: 'com.exponative.windtemplatestorybook',
+		'infoPlist': {
+			'ITSAppUsesNonExemptEncryption': false,
+		},
+	},
+	android: {
+		adaptiveIcon: {
+			foregroundImage: './assets/adaptive-icon.png',
+			backgroundColor: '#FFFFFF',
+		},
+	},
+	web: {
+		favicon: './assets/favicon.png',
+	},
+	'updates': {
+		'url': 'https://u.expo.dev/fd290ea5-8636-4cf1-965d-0da59c604ee0',
+	},
+	'runtimeVersion': {
+		'policy': 'appVersion',
+	},
 });
